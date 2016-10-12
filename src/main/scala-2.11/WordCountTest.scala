fb7476d86873afb87f38bc9c6310787fe6fbd1b6
Jesse <jesse.prestwoodtaylor@gmail.com>
2016-10-12 12:51:29 -0400

    added additional spark dependencies, added test word count file to ensure Spark runs as expected

--- src/main/scala-2.11/WordCountTest.scala
+++ src/main/scala-2.11/WordCountTest.scala
@@ -2,4 +2,21 @@
   * Created by jesseprestwood-taylor on 10/12/16.
   */
 
+import org.apache.spark.{SparkContext, SparkConf}
+object WordCountTest {
+  def main(args: Array[String]) = {
+    val conf = new SparkConf()
+      .setMaster("local[*]")
+      .setAppName("Test Spark")
+      .set("spark.executor.memory", "2g")
 
+    val sc = new SparkContext(conf)
+    val lines = sc.parallelize(Seq("This is the first sentence", " This is the second sentence",
+      " This is the third sentence"))
+    val counts = lines.flatMap(line => line.split(" "))
+      .map(word => (word, 1))
+      .reduceByKey(_+_)
+
+    counts.foreach(println)
+  }
+}
